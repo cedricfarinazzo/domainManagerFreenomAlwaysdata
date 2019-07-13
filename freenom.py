1f74c5aa1f3c066dc3ff5cb1a155abf7f124c438
cedric.farinazzo <cedric.farinazzo@epita.fr>
2019-07-13 23:25:04 +0200

    add renew freenom domain
    Signed-off-by: cedric.farinazzo <cedric.farinazzo@epita.fr>

--- freenom.py
+++ freenom.py
@@ -35,6 +35,10 @@ class DomainCheckerFreenomDNS(checker.DomainChecker):
 
 class DomainCheckerFreenomAPI(checker.DomainChecker):
 
+    def __init__(self, *args, **kwargs):
+        super(DomainCheckerFreenomAPI, self).__init__(*args, **kwargs)
+        self.freenom = freenom_dns_updater.Freenom()
+
     def __domain_in_config(self, domainname):
         for d in self.domains:
             if d.name == domainname:
@@ -42,40 +46,54 @@ class DomainCheckerFreenomAPI(checker.DomainChecker):
         return False
 
     def check(self, login, password):
-        freenom = freenom_dns_updater.Freenom()
-        if freenom.login(login, password):
-            add_to_config = []
-            need_renew = []
-            domains_list = freenom.list_domains()
-            for d in domains_list:
-                print('Domain %s : ' % (d.name), end='  ')
-                days_before_renew = (d.expire_date - date.today()).days
-                ok = True
-                if not self.__domain_in_config(d.name):
-                    add_to_config.append(d.name)
-                    print(' Not found in your config file', end=' ')
-                    ok = False
-                if freenom.need_renew(d):
-                    need_renew.append(d.name)
-                    print(' Need renew :', days_before_renew,
-                          "days", end=' ')
-                    ok = False
-                if ok:
-                    print(' OK (', days_before_renew, 'days before renew)',
-                          end='')
-                print()
-            try:
-                self.to_do["add_to_config"] = self.to_do["add_to_config"] \
-                        + add_to_config
-            except Exception:
-                self.to_do["add_to_config"] = add_to_config
-            try:
-                self.to_do["need_renew"] = self.to_do["need_renew"] \
-                        + need_renew
-            except Exception:
-                self.to_do["need_renew"] = need_renew
-        else:
-            print("Connection failed to the Freenom API")
+        try:
+            if not self.freenom.login(login, password):
+                raise Exception()
+            domains_list = self.freenom.list_domains()
+        except Exception:
+            print("Freenom API: Bad credentials. Cannot get domains list")
+            return
+        add_to_config = []
+        need_renew = []
+        for d in domains_list:
+            print('Domain %s : ' % (d.name), end='  ')
+            days_before_renew = (d.expire_date - date.today()).days
+            ok = True
+            if not self.__domain_in_config(d.name):
+                add_to_config.append(d.name)
+                print(' Not found in your config file', end=' ')
+                ok = False
+            if self.freenom.need_renew(d):
+                need_renew.append(d.name)
+                print(' Need renew :', days_before_renew,
+                      "days", end=' ')
+                ok = False
+            if ok:
+                print(' OK (', days_before_renew, 'days before renew)',
+                      end='')
+            print()
+        try:
+            self.to_do["add_to_config"] = self.to_do["add_to_config"] \
+                    + add_to_config
+        except Exception:
+            self.to_do["add_to_config"] = add_to_config
+        try:
+            self.to_do["need_renew"] = self.to_do["need_renew"] \
+                    + need_renew
+        except Exception:
+            self.to_do["need_renew"] = need_renew
 
     def update(self):
-        pass
+        try:
+            domains_list = self.freenom.list_domains()
+        except Exception:
+            print("Freenom API: failed to get domain list")
+            return
+        for d in domains_list:
+            if d.name in self.to_do["need_renew"]:
+                try:
+                    if not self.freenom.renew(d):
+                        raise Exception()
+                    print("Renew ", d.name)
+                except Exception:
+                    print("Failed to renew ", d.name)
